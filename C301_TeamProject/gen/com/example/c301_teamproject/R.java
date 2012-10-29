020446b02c594bfaa98eef0496cd63d2a5a1ca82
Kris Kushniruk <kkushnir@ualberta.ca>
2012-10-29 15:11:40 -0600

    Just another change

--- C301_TeamProject/gen/com/example/c301_teamproject/R.java
+++ C301_TeamProject/gen/com/example/c301_teamproject/R.java
@@ -17,17 +17,16 @@ public final class R {
     public static final class id {
         public static final int addItemToTask=0x7f070006;
         public static final int button1=0x7f070000;
-        public static final int button2=0x7f07000b;
-        public static final int button3=0x7f07000c;
+        public static final int button2=0x7f07000a;
+        public static final int button3=0x7f07000b;
         public static final int desiredNum=0x7f070002;
         public static final int editText1=0x7f070007;
         public static final int listView1=0x7f070005;
-        public static final int menu_settings=0x7f07000d;
+        public static final int menu_settings=0x7f07000c;
         public static final int saveTask=0x7f070009;
         public static final int textView1=0x7f070001;
         public static final int textView2=0x7f070003;
         public static final int titleTextBox=0x7f070004;
-        public static final int togglePrivate=0x7f07000a;
         public static final int togglePublic=0x7f070008;
     }
     public static final class layout {
