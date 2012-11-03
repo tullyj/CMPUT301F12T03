52b28ab0ed61bdbc5d2b88e1e76cbca75be839c0
Kris Kushniruk <kkushnir@ualberta.ca>
2012-11-03 15:09:05 -0600

    Added attempting to display the tasks

--- C301_TeamProject/src/ca/ualberta/cs/c301_teamproject/ViewTasks.java
+++ C301_TeamProject/src/ca/ualberta/cs/c301_teamproject/ViewTasks.java
@@ -1,9 +1,15 @@
 package ca.ualberta.cs.c301_teamproject;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.Toast;
+import ca.ualberta.cs.c301_interfaces.Task;
+import ca.ualberta.cs.c301_repository.TfTaskRepository;
 
 public class ViewTasks extends Activity {
 
@@ -11,6 +17,39 @@ public class ViewTasks extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_tasks);
+        
+    	Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, "test", duration);
+		toast.show();
+        
+        
+        //testing grabbing tasks
+        try {
+			List<Task> taskList = TfTaskRepository.getAllTasks();
+			
+			for(Task task: taskList){
+				
+
+
+				toast = Toast.makeText(context, task.getTitle(), duration);
+				toast.show();
+				
+				
+				
+			}
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+        
     }
 
     @Override
