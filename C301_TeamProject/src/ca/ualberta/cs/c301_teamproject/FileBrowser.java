b7bbea4ff78fdf35f7741d204e1803bdbcc47c2f
Edwin Chung <edwin.chung@ualberta.ca>
2012-11-04 18:04:01 -0700

    Trying to upload the missing files

---
+++ C301_TeamProject/src/ca/ualberta/cs/c301_teamproject/FileBrowser.java
@@ -0,0 +1,205 @@
+package ca.ualberta.cs.c301_teamproject;
+
+import java.io.File;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+import android.app.AlertDialog;
+
+import android.app.ListActivity;
+
+import android.content.DialogInterface;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+
+import android.widget.ListView;
+
+import android.widget.TextView;
+
+
+
+public class FileBrowser extends ListActivity {
+
+ 
+
+ private List<String> item = null;
+
+ private List<String> path = null;
+
+ private String root="/";
+
+ private TextView myPath;
+
+ //http://android-er.blogspot.ca/2010/01/implement-simple-file-explorer-in.html
+
+    /** Called when the activity is first created. */
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.file_browser);
+
+        myPath = (TextView)findViewById(R.id.path);
+
+        getDir(root);
+
+    }
+
+    
+
+    private void getDir(String dirPath)
+
+    {
+
+     myPath.setText("Location: " + dirPath);
+
+     
+
+     item = new ArrayList<String>();
+
+     path = new ArrayList<String>();
+
+     
+
+     File f = new File(dirPath);
+
+     File[] files = f.listFiles();
+
+     
+
+     if(!dirPath.equals(root))
+
+     {
+
+
+
+      item.add(root);
+
+      path.add(root);
+
+      
+
+      item.add("../");
+
+      path.add(f.getParent());
+
+            
+
+     }
+
+     
+
+     for(int i=0; i < files.length; i++)
+
+     {
+
+       File file = files[i];
+
+       path.add(file.getPath());
+
+       if(file.isDirectory())
+
+        item.add(file.getName() + "/");
+
+       else
+
+        item.add(file.getName());
+
+     }
+
+
+
+     ArrayAdapter<String> fileList =
+
+      new ArrayAdapter<String>(this, R.layout.row, item);
+
+     setListAdapter(fileList);
+
+    }
+
+
+
+ @Override
+
+ protected void onListItemClick(ListView l, View v, int position, long id) {
+
+  
+
+  File file = new File(path.get(position));
+
+  
+
+  if (file.isDirectory())
+
+  {
+
+   if(file.canRead())
+
+    getDir(path.get(position));
+
+   else
+
+   {
+
+    new AlertDialog.Builder(this)
+
+    .setIcon(R.drawable.taskforcebar)
+
+    .setMessage("[" + file.getName() + "] folder can't be read!")
+
+    .setPositiveButton("OK", 
+
+      new DialogInterface.OnClickListener() {
+
+
+       public void onClick(DialogInterface dialog, int which) {
+
+        // TODO Auto-generated method stub
+
+       }
+
+      }).show();
+
+   }
+
+  }
+
+  else
+
+  {
+
+   new AlertDialog.Builder(this)
+
+    .setIcon(R.drawable.taskforcebar)
+
+    .setMessage("[" + file.getName() + "]")
+
+    .setPositiveButton("OK", 
+
+      new DialogInterface.OnClickListener() {
+
+       
+
+       
+       public void onClick(DialogInterface dialog, int which) {
+
+        // TODO Auto-generated method stub
+
+       }
+
+      }).show();
+
+  }
+
+ }
+
+}
---
+++ C301_TeamProject/src/ca/ualberta/cs/c301_teamproject/InputText.java
@@ -0,0 +1,69 @@
+package ca.ualberta.cs.c301_teamproject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class InputText extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.input_text, menu);
+        return true;
+    }
+    
+    public void saveChanges(View view) {
+    	String inputText = "";
+    	
+    	//hardcoded filename for now, going to be getNumber() + 1?
+    	String fileName = "/001";
+    	
+    	//hardcoded file directory for now, may be changed later
+    	String directory = Environment.getExternalStorageDirectory().toString()
+    			+ "/TextFiles";
+
+    	EditText et = (EditText) findViewById(R.id.text_input_box);
+    	inputText = et.getText().toString();
+    	
+    	//create the directory, if it does not exist
+    	File folder = new File(directory);
+    	if(!folder.exists()){
+    		//Toast if created
+    		if(folder.mkdirs()){
+    			Toast.makeText(getBaseContext(), "folder Created", 
+        				Toast.LENGTH_SHORT).show();
+    		}
+    	}
+    	//Toast if not created
+    	else
+    		Toast.makeText(getBaseContext(), "folder not Created", 
+    				Toast.LENGTH_SHORT).show();
+    	//create and write the text into the saved file
+    	try {
+    		File myTextFile = new File(directory + fileName + ".txt");
+    		    		
+    		myTextFile.createNewFile();
+    		FileOutputStream fOut = new FileOutputStream(myTextFile);
+    		OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+    		myOutWriter.append(inputText);
+    		myOutWriter.close();
+    	} catch (Exception e) {
+    		Toast.makeText(getBaseContext(), e.getMessage(), 
+    				Toast.LENGTH_SHORT).show();
+    	}
+    }
+}
