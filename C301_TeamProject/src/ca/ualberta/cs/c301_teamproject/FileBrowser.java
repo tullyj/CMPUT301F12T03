d692855883b9191c6dd3aa14e1db8e4f510b1a19
Edwin Chung <edwin.chung@ualberta.ca>
2012-11-21 09:37:34 -0700

    Added a filetype checking

--- C301_TeamProject/src/ca/ualberta/cs/c301_teamproject/FileBrowser.java
+++ C301_TeamProject/src/ca/ualberta/cs/c301_teamproject/FileBrowser.java
@@ -2,6 +2,7 @@ package ca.ualberta.cs.c301_teamproject;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import android.app.AlertDialog;
@@ -13,18 +14,29 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FileBrowser extends ListActivity {
- private List<String> item = null;
- private List<String> path = null;
- private String root="/";
- private TextView myPath;
+private List<String> item = null;
+private List<String> path = null;
+private String root="/";
+private TextView myPath;
+
+static private int id;
+
+final private List<String> imageExtensions = 
+		 Arrays.asList("png", "jpg", "jpeg", "gif", "bmp");
+final private List<String> audioExtensions =
+		 Arrays.asList("wav", "mid", "mp3", "wma", "flac", "ogg");
+final private List<String> videoExtensions =
+		 Arrays.asList("avi", "mp4", "wmv");
 
     /**
      * This class is a modified version of the code given by Androider+ in the
      * following URL:
      * 
-     * http://android-er.blogspot.ca/2010/01/implement-simple-file-explorer-in.html
+     * http://android-er.blogspot.ca/2010/01
+     * 								/implement-simple-file-explorer-in.html
      * 
      * @author Edwin Chung
      */
@@ -36,6 +48,7 @@ public class FileBrowser extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.file_browser);
         myPath = (TextView)findViewById(R.id.path);
+        id = getIntent().getIntExtra("FileType", 0);
         getDir(root);
     }
 
@@ -108,18 +121,58 @@ public class FileBrowser extends ListActivity {
             new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int which) {
                     File addFile = file.getAbsoluteFile();
-                    InputFile.files.add(addFile);
-    	   
-                    //go back to input list
-//                    Intent intent = new Intent(getApplicationContext(), 
-//                        InputFile.class);
-                    Intent intent = getIntent();
-                    intent.putExtra("FromFile", 4);
-                    setResult(RESULT_OK, intent);
-                    finish();
-                    //startActivity(intent);
+                    if(checkExt(addFile.getName())){
+                    	InputFile.files.add(addFile);
+                        //go back to input list
+                        Intent intent = new Intent(getApplicationContext(), 
+                            InputFile.class);
+                        intent.putExtra("FromFile", 4);
+                    	startActivity(intent);
+                    	/*
+                    	Intent intent = getIntent();
+                    	intent.putExtra("FromFile", 4);
+                    	setResult(RESULT_OK, intent);
+                    	finish();
+                    	*/
+                        
+                    }
+                    else
+                		Toast.makeText(getApplicationContext(), 
+                				"Incompatible file type", 
+                				Toast.LENGTH_LONG).show();
+
                 }
             }).show();
             }
     }
+     
+    public boolean checkExt(String fileName){
+    	String ext = getExt(fileName).toLowerCase();
+    	
+    	if(audioExtensions.contains(ext) && id == 1)
+    		return true;
+    	else if(imageExtensions.contains(ext) && id == 2)
+    		return true;
+    	else if(videoExtensions.contains(ext) && id == 5)
+    		return true;
+    	else
+    		return false;
+    }
+    
+    public String getExt(String fileName){
+    	String ext = "";
+    	String temp = fileName;
+    	int period_pos = temp.lastIndexOf('.');
+    	//if the file name has no periods
+    	if(period_pos == -1)
+    		return ext;
+    	else {
+    		//extract extension
+    		ext = temp.substring(period_pos + 1);
+//    		Toast.makeText(getApplicationContext(), 
+//    				ext, Toast.LENGTH_LONG).show();
+    	}
+    	return ext;
+    }
 }
+
