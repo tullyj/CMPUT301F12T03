6cdea534a8a5789105a679ce5ea36189f6db7d36
Tully Johnson <tullyjohnson@gmail.com>
2012-11-24 17:22:13 -0700

    same as last commit, just missed a couple files last time...

---
+++ C301_TeamProject/src/ca/ualberta/cs/c301_preview/PreviewText.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cs.c301_preview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreviewText extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+    }
+}
---
+++ C301_TeamProject/src/ca/ualberta/cs/c301_preview/PreviewVideo.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cs.c301_preview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreviewVideo extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+    }
+}
