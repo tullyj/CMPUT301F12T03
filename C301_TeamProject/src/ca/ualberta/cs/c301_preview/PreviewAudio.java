4f0dc83c765d07a9435e88fbc560eac4fbb32261
Tully Johnson <tullyjohnson@gmail.com>
2012-11-24 17:52:44 -0700

    updating previewaudio

--- C301_TeamProject/src/ca/ualberta/cs/c301_preview/PreviewAudio.java
+++ C301_TeamProject/src/ca/ualberta/cs/c301_preview/PreviewAudio.java
@@ -22,20 +22,12 @@ import java.io.IOException;
 public class PreviewAudio extends Activity {
 	
 	private static final String LOG_TAG = "PreviewAudio";
-    private static String mFileName = null;
-
-//    private RecordButton mRecordButton = null;
-//    private MediaRecorder mRecorder = null;
+    //private static String mFileName = null;
+    
+    private static File file;
 
     private PlayButton   mPlayButton = null;
     private MediaPlayer   mPlayer = null;
-    
-//    private SaveButton   mSaveButton = null;
-
-//	private void onRecord(boolean start) {
-//		if(start) startRecording();
-//		else stopRecording();
-//	}
 	
 	private void onPlay(boolean start) {
 		if (start) startPlaying();
@@ -45,7 +37,7 @@ public class PreviewAudio extends Activity {
 	private void startPlaying() {
 		mPlayer = new MediaPlayer();
 		try {
-			mPlayer.setDataSource(mFileName);
+			mPlayer.setDataSource(file.getAbsolutePath());
 			mPlayer.prepare();
 			mPlayer.start();
 		} catch (IOException e) {
@@ -57,50 +49,6 @@ public class PreviewAudio extends Activity {
 		mPlayer.release();
 		mPlayer = null;
 	}
-	
-//	private void startRecording() {
-//		mRecorder = new MediaRecorder();
-//		mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-//		mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-//		mRecorder.setOutputFile(mFileName);
-//		mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//		
-//		try {
-//			mRecorder.prepare();
-//		} catch (IOException e) {
-//			Log.e(LOG_TAG, "prepare() failed");
-//		}
-//		
-//		mRecorder.start();
-//	}
-	
-//	private void stopRecording() {
-//		mRecorder.stop();
-//		mRecorder.release();
-//		mRecorder = null;
-//	}
-	
-//	class RecordButton extends Button {
-//        boolean mStartRecording = true;
-//
-//        OnClickListener clicker = new OnClickListener() {
-//            public void onClick(View v) {
-//                onRecord(mStartRecording);
-//                if (mStartRecording) {
-//                    setText("Stop recording");
-//                } else {
-//                    setText("Start recording");
-//                }
-//                mStartRecording = !mStartRecording;
-//            }
-//        };
-//
-//        public RecordButton(Context ctx) {
-//            super(ctx);
-//            setText("Start recording");
-//            setOnClickListener(clicker);
-//        }
-//    }
 
     class PlayButton extends Button {
         boolean mStartPlaying = true;
@@ -123,69 +71,29 @@ public class PreviewAudio extends Activity {
             setOnClickListener(clicker);
         }
     }
-    
-//    class SaveButton extends Button {
-//        OnClickListener clicker = new OnClickListener() {
-//            public void onClick(View v) {
-//                onSave();
-//            }
-//        };
-//
-//        public SaveButton(Context ctx) {
-//            super(ctx);
-//            setText(R.string.save_changes);
-//            setOnClickListener(clicker);
-//        }
-//    }
-    
-//    public PreviewAudio() {
-//        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-//        mFileName += "/audiorecordtest.3gp";
-//    }
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         LinearLayout ll = new LinearLayout(this);
-//        mRecordButton = new RecordButton(this);
-//        ll.addView(mRecordButton,
-//            new LinearLayout.LayoutParams(
-//                ViewGroup.LayoutParams.WRAP_CONTENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT, 0));
         mPlayButton = new PlayButton(this);
         ll.addView(mPlayButton,
             new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT, 0));
-//        mSaveButton = new SaveButton(this);
-//        ll.addView(mSaveButton,
-//                new LinearLayout.LayoutParams(
-//                    ViewGroup.LayoutParams.WRAP_CONTENT,
-//                    ViewGroup.LayoutParams.WRAP_CONTENT, 0));
         setContentView(ll);
-    }
 
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        if (mRecorder != null) {
-//            mRecorder.release();
-//            mRecorder = null;
-//        }
-//
-//        if (mPlayer != null) {
-//            mPlayer.release();
-//            mPlayer = null;
-//        }
-//    }
-	
-    private void onSave() {
-    	//Intent mIntent = parseUri(Uri.fromFile(new File(mFileName)), 0);
-    	//Intent mIntent = getIntent();
-    	//mIntent.putExtra("AudioExtra", Uri.fromFile(new File(mFileName)));
-    	
-    	//setResult(RESULT_OK, mIntent);
-    	finish();
+        Intent intent = getIntent();
+        file = new File(intent.getData().getPath());
     }
+	
+//    private void onSave() {
+//    	//Intent mIntent = parseUri(Uri.fromFile(new File(mFileName)), 0);
+//    	//Intent mIntent = getIntent();
+//    	//mIntent.putExtra("AudioExtra", Uri.fromFile(new File(mFileName)));
+//    	
+//    	//setResult(RESULT_OK, mIntent);
+//    	finish();
+//    }
 }
