bed824b197144daa4fea568e4f88c3702e217ca3
Edwin Chung <edwin.chung@ualberta.ca>
2012-12-01 14:00:26 -0700

    Preview Photo with a black background

--- C301_TeamProject/src/ca/ualberta/cs/c301_preview/PreviewPhoto.java
+++ C301_TeamProject/src/ca/ualberta/cs/c301_preview/PreviewPhoto.java
@@ -1,11 +1,16 @@
 package ca.ualberta.cs.c301_preview;
 
+import java.io.File;
+
+import ca.ualberta.cs.c301_teamproject.ItemList;
 import ca.ualberta.cs.c301_teamproject.R;
 import ca.ualberta.cs.c301_teamproject.R.layout;
 import ca.ualberta.cs.c301_teamproject.R.menu;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class PreviewPhoto extends Activity {
 
@@ -13,7 +18,13 @@ public class PreviewPhoto extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preview_picture);
+        File file = ItemList.currFile;
         
+        ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        
+        Uri mUri = Uri.fromFile(file);
+        
+        iv.setImageURI(mUri);
     }
 
     @Override
